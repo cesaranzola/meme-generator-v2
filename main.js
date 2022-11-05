dfd911a010c6ba19b993e787a53bdd9bec45a61e
deus <deus@deus.local>
2022-11-04 20:33:14 -0500

    Add highlight class when a radio butto is press

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ import { catsData } from './data.js';
 
 const emotionsBtns = document.getElementById('emotions-buttons');
 
+emotionsBtns.addEventListener('change', highlight);
+
+function highlight(e) {
+	const radioButtons = document.getElementsByClassName('radio');
+	for (let radio of radioButtons) {
+		radio.classList.remove('highlight');
+	}
+	document
+		.getElementById(e.target.id)
+		.parentElement.classList.add('highlight');
+}
+
 function getEmotions(cats) {
 	let emotionsArray = [];
 	for (let cat of cats) {
@@ -17,14 +29,15 @@ function getEmotions(cats) {
 function renderEmotionsButtons(cats) {
 	const emotions = getEmotions(cats);
 	for (let emotion of emotions) {
-		emotionsBtns.innerHTML += `	<div>
+		emotionsBtns.innerHTML += `	<div class="radio radioBox container-fluid">
+            <label for="${emotion}">${emotion}</label>
 			<input
 				type="radio"
 				name="radio"
 				value="${emotion}"
 				id="${emotion}"
 			/>
-			<label for="${emotion}">${emotion}</label>
+
 		</div>`;
 	}
 }
