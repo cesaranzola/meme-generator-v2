0e2ad0ba81646849ccf1ad3aca4ae21c43154bf5
deus <deus@deus.local>
2022-11-04 23:36:36 -0500

    Add final app

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ import { catsData } from './data.js';
 
 const emotionsBtns = document.getElementById('emotions-buttons');
 const popBox = document.getElementById('popBox');
+const gifCheck = document.getElementById('gif-checkbox');
+const getImageBtn = document.getElementById('get-image');
+const imageContainer = document.getElementById('imageContainer');
+const closeBtn = document.getElementById('close-button');
 
 emotionsBtns.addEventListener('change', highlight);
+closeBtn.addEventListener('click', closeBox);
+getImageBtn.addEventListener('click', renderCat);
 
 function highlight(e) {
 	const radioButtons = document.getElementsByClassName('radio');
@@ -15,20 +21,61 @@ function highlight(e) {
 		.parentElement.classList.add('highlight');
 }
 
-function renderCat() {
-	const object = getSingleObject();
-	popBox.innerHTML += `<img
-			src="images/${object.image}"
-			alt="${object.alt}"
-			class="img-fluid"
-			style="width: 400px; height: 400px"
-		/>`;
-	popBox.style.display = 'flex';
+function closeBox() {
+	popBox.style.display = 'none';
+	imageContainer.innerHTML = '';
 }
 
-function getSingleObject() {}
+function renderCat() {
+	const object = getSingleObject();
+	imageContainer.innerHTML += `
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div
+						class="d-flex justify-content-center align-items-center"
+					>
+						<img
+							src="images/${object.image}"
+							alt="${object.alt}"
+							class="img-fluid rounded"
+							style="width: 200px; height: 200px; margin-top: 8rem;"
+						/>
+					</div>
+				</div>
+			</div>
+		</div>`;
+	popBox.style.display = 'block';
+}
 
-function getMatchingArray() {}
+function getSingleObject() {
+	const objectArr = getMatchingArray();
+	const arrLen = objectArr.length;
+
+	if (arrLen === 1) {
+		return objectArr[0];
+	} else {
+		let randNum = Math.floor(Math.random() * arrLen);
+		return objectArr[randNum];
+	}
+}
+
+function getMatchingArray() {
+	if (document.querySelector('input[type=radio]:checked')) {
+		const selectedOption = document.querySelector(
+			'input[type=radio]:checked'
+		).value;
+		const isGif = gifCheck.checked;
+		const matchingArr = catsData.filter((cat) => {
+			if (isGif) {
+				return cat.emotionTags.includes(selectedOption) && cat.isGif;
+			} else {
+				return cat.emotionTags.includes(selectedOption);
+			}
+		});
+		return matchingArr;
+	}
+}
 
 function getEmotions(cats) {
 	let emotionsArray = [];
